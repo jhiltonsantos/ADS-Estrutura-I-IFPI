248090d6c3d8b4fdededfca84c38d1fa6cf42a29
Hilton Santos <jhilton930@gmail.com>
2019-08-27 22:40:12 -0300

    add fabio_01_01

--- fabio_01/fabio_01_01_juncao_de_vetores.c
+++ fabio_01/fabio_01_01_juncao_de_vetores.c
@@ -2,3 +2,46 @@
 // Created by hilton on 27/08/2019.
 //
 
+#include <stdio.h>
+
+int main(){
+    int quantA, quantB, soma;
+    printf("Determine quantidade de posicoes no vetor A: ");
+    scanf("%d", &quantA);
+    printf("Determine quantidade de posicoes no vetor B: ");
+    scanf("%d", &quantB);
+
+    printf("\n");
+    int vetorA[quantA], vetorB[quantB];
+
+    for (int i=0;i<quantA;i++){
+        printf("Digite o valor na posicao %d no vetor A: ", i+1);
+        scanf("%d", &vetorA[i]);
+    }
+    printf("\n");
+    for (int i=0;i<quantB;i++){
+        printf("Digite o valor na posicao %d no vetor B: ", i+1);
+        scanf("%d", &vetorB[i]);
+    }
+
+    soma = quantA + quantB;
+    int vetorC[soma];
+
+    for (int i=0; i<quantA;i++){
+        vetorC[i] = vetorA[i];
+    }
+    for (int i=0; i<quantB;i++){
+        vetorC[i+quantA]= vetorB[i];
+    }
+
+    printf("\n");
+
+    for (int i=0; i<soma;i++){
+        printf("Valor na posicao %d: %d\n", i+1,vetorC[i]);
+    }
+
+    return 0;
+}
+
+
+
