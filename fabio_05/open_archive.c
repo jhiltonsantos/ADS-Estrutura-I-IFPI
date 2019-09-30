48b59baaf7072ba4400a11a3155d9cf32e10b6eb
Hilton Santos <jhilton930@gmail.com>
2019-09-30 11:59:00 -0300

    Add Fabio 05

---
+++ fabio_05/open_archive.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main()
+{
+
+    FILE *arquivo;
+    char frase[80], nomeDoArquivo[20];
+
+    system("cls");
+    printf("Nome do arquivo: ");
+    fflush(stdin);
+    gets(nomeDoArquivo);
+
+    arquivo = fopen(nomeDoArquivo, "r");
+    if (arquivo == NULL)
+    {
+        printf("Erro na abertura do Arquivo : %s. Tecle algo !\n", nomeDoArquivo);
+        getch();
+    }
+    else
+    {
+        printf("\n\n");
+        fgets(frase, 80, arquivo);
+        while (!feof(arquivo))
+        {
+            printf("%s", frase);
+            fgets(frase, 80, arquivo);
+        }
+        fclose(arquivo);
+        printf("\n\n *** fim de texto *** Tecle algo !");
+        getch();
+    }
+}
