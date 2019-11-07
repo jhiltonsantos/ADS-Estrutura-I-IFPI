fda95cb78c0d96c71746484672a9ae53cfdd52d1
Hilton Santos <jhilton930@gmail.com>
2019-11-06 22:01:21 -0300

    Update Shaker Sort

--- fabio_07/shaker_sort.c
+++ fabio_07/shaker_sort.c
@@ -3,15 +3,29 @@
 #include <stdbool.h>
 #include <time.h>
 
+void mostrar_array(int tam, int array[tam])
+{
+    for (int i = 0; i < tam; i++)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+
 void shaker_sort(int tam, int array[tam])
 {
     int inferior = 0;
     int topo = tam - 1;
     bool continua = true;
     int aux;
+    int volta = 1;
+
+    printf("\nAntes do Shaker Sort: \n");
+    mostrar_array(tam, array);
 
     do
     {
+        printf("\nVolta %d: \n", volta);
         continua = false;
 
         for (int i = inferior; i < topo; i++)
@@ -22,10 +36,12 @@ void shaker_sort(int tam, int array[tam])
                 array[i] = array[i + 1];
                 array[i + 1] = aux;
                 continua = true;
+                mostrar_array(tam, array);
             }
         }
+        
         topo--;
-
+        
         for (int i = topo; i > inferior; i--)
         {
             if (array[i] < array[i - 1])
@@ -34,38 +50,33 @@ void shaker_sort(int tam, int array[tam])
                 array[i] = array[i - 1];
                 array[i - 1] = aux;
                 continua = true;
+                mostrar_array(tam, array);
             }
         }
+        
         inferior++;
-
+        volta++;
+        
     } while (continua == true && inferior < topo);
+
+    printf("\nDepois do Shaker Sort: \n");
+    mostrar_array(tam, array);
 }
 
-void mostrar_array(int tam, int array[tam])
-{
-    for (int i = 0; i < 10; i++)
-    {
-        printf("%d ", array[i]);
-    }
-    printf("\n");
-}
 
 int main(void)
 {
-    srand(time(NULL)); // semente
+    srand(time(NULL));
 
-    int tam = 10;
+    int tam = 5;
     int array[tam];
+    //int array[5] = {5,4,2,3,1};
 
     for (int i = 0; i < tam; i++)
     {
-        array[i] = rand() % 100;
+        array[i] = rand() % 5;
     }
 
-    printf("\nAntes do Shaker Sort: \n");
-    mostrar_array(tam, array);
-
     shaker_sort(tam, array);
-    printf("\nDepois do Shaker Sort: \n");
-    mostrar_array(tam, array);
+    //shaker_sort(5, array);
 }
